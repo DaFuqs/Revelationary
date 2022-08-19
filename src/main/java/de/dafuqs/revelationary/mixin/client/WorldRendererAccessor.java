674d99e878770a06a985c58f8227d10a62a20056
DaFuqs <dominik.m.schmidt@googlemail.com>
2022-08-19 18:54:32 +0200

    More preceise handling of block drops in combination with other mods. Fixed trying to load a mixin on dedicated servers for a class that does not exist.

---
+++ src/main/java/de/dafuqs/revelationary/mixin/client/WorldRendererAccessor.java
@@ -0,0 +1,13 @@
+package de.dafuqs.revelationary.mixin.client;
+
+import net.minecraft.client.render.WorldRenderer;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Invoker;
+
+@Mixin(WorldRenderer.class)
+public interface WorldRendererAccessor {
+    
+    @Invoker("scheduleChunkRender")
+    void invokeScheduleChunkRender(int x, int y, int z, boolean important);
+    
+}
