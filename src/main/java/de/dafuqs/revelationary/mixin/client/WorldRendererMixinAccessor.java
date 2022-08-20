b088ba9ae635c116fb7aec8b118b24523ba3211c
DaFuqs <dominik.m.schmidt@googlemail.com>
2022-08-20 14:31:01 +0200

    cursed sodium compat

--- src/main/java/de/dafuqs/revelationary/mixin/client/WorldRendererMixinAccessor.java
+++ src/main/java/de/dafuqs/revelationary/mixin/client/WorldRendererMixinAccessor.java
@@ -5,7 +5,7 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.gen.Invoker;
 
 @Mixin(WorldRenderer.class)
-public interface WorldRendererAccessor {
+public interface WorldRendererMixinAccessor {
     
     @Invoker("scheduleChunkRender")
     void invokeScheduleChunkRender(int x, int y, int z, boolean important);
