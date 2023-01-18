b7b66bfb69560575e5dd87e868d4d62a33d342f3
DaFuqs <dominik.m.schmidt@googlemail.com>
2023-01-18 20:07:31 +0100

    added a config to specify cloaked block / item names
    # Conflicts:
    #	gradle.properties

---
+++ src/main/java/de/dafuqs/revelationary/config/RevelarionaryConfig.java
@@ -0,0 +1,19 @@
+package de.dafuqs.revelationary.config;
+
+import me.shedaniel.autoconfig.*;
+import me.shedaniel.autoconfig.annotation.*;
+import me.shedaniel.cloth.clothconfig.shadowed.blue.endless.jankson.*;
+
+@Config(name = "Revelationary")
+public class RevelarionaryConfig implements ConfigData {
+	
+	@Comment("Use target block/item name instead of scattering it")
+	public boolean UseTargetBlockOrItemNameInsteadOfScatter = false;
+
+	@Comment("Name for cloaked Blocks")
+	public String NameForCloakedBlocks = "";
+
+	@Comment("Name for cloaked Items")
+	public String NameForCloakedItems = "item.minecraft.poppy";
+	
+}
