cfa18c455fa63a09f2292302a524147f78dbb25c
DaFuqs <dominik.m.schmidt@googlemail.com>
2023-01-23 19:53:50 +0100

    yeet cloth config

--- src/main/java/de/dafuqs/revelationary/config/RevelarionaryConfig.java
+++ src/main/java/de/dafuqs/revelationary/config/RevelarionaryConfig.java
@@ -1,19 +1,61 @@
 package de.dafuqs.revelationary.config;
 
-import me.shedaniel.autoconfig.*;
-import me.shedaniel.autoconfig.annotation.*;
-import me.shedaniel.cloth.clothconfig.shadowed.blue.endless.jankson.*;
+import com.google.gson.*;
+import de.dafuqs.revelationary.*;
+import net.fabricmc.loader.api.FabricLoader;
+import net.minecraft.util.*;
 
-@Config(name = "Revelationary")
-public class RevelarionaryConfig implements ConfigData {
-	
-	@Comment("Use target block/item name instead of scattering it")
+import java.io.*;
+
+public class RevelarionaryConfig {
+
+	private static final Gson GSON = new GsonBuilder().setPrettyPrinting().disableHtmlEscaping().create();
+
+	public boolean UnrevealedBlocksAreUnbreakable = false;
 	public boolean UseTargetBlockOrItemNameInsteadOfScatter = false;
+	public String NameForUnrevealedBlocks = "";
+	public String NameForUnrevealedItems = "";
 
-	@Comment("Name for cloaked Blocks")
-	public String NameForCloakedBlocks = "";
+	public RevelarionaryConfig() {
+		File configFile = FabricLoader.getInstance().getConfigDir().resolve("Revelationary.json").toFile();
+		FileReader configReader;
+		try {
+			configReader = new FileReader(configFile);
+		} catch (Exception e) {
+			try {
+				configFile.createNewFile();
+				FileWriter myWriter = new FileWriter(configFile);
+				myWriter.write(DEFAULT_CONFIG);
+				myWriter.close();
 
-	@Comment("Name for cloaked Items")
-	public String NameForCloakedItems = "";
+				configReader = new FileReader(configFile);
+			} catch (IOException ioException) {
+				Revelationary.logError("Could not generate config file under " + configFile.getAbsolutePath() + ".\n" + ioException.getLocalizedMessage());
+				return;
+			}
+		}
+
+		JsonObject jsonObject;
+		try {
+			jsonObject = JsonHelper.deserialize(GSON, configReader, JsonObject.class);
+		} catch (Exception e) {
+			Revelationary.logError("Could not parse Revelationary.json: " + e.getLocalizedMessage());
+			return;
+		}
+
+		UnrevealedBlocksAreUnbreakable = JsonHelper.getBoolean(jsonObject, "UnrevealedBlocksAreUnbreakable", false);
+		UseTargetBlockOrItemNameInsteadOfScatter = JsonHelper.getBoolean(jsonObject, "UseTargetBlockOrItemNameInsteadOfScatter", false);
+		NameForUnrevealedBlocks = JsonHelper.getString(jsonObject, "NameForUnrevealedBlocks", "");
+		NameForUnrevealedItems = JsonHelper.getString(jsonObject, "NameForUnrevealedItems", "");
+	}
+
+	private static final String DEFAULT_CONFIG = """
+{
+	"UnrevealedBlocksAreUnbreakable": false,
+	"UseTargetBlockOrItemNameInsteadOfScatter": false,
+	"NameForUnrevealedBlocks": "",
+	"NameForUnrevealedItems": ""
+}
+""";
 	
 }
