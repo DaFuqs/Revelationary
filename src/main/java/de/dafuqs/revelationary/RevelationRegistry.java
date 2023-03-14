616679dcfabc8bc73c2b6b206d0f4346ea4da211
DaFuqs <dominik.m.schmidt@googlemail.com>
2023-03-14 14:00:16 +0100

    do not report cloaking as air (totally valid)

--- src/main/java/de/dafuqs/revelationary/RevelationRegistry.java
+++ src/main/java/de/dafuqs/revelationary/RevelationRegistry.java
@@ -159,7 +159,7 @@ public class RevelationRegistry {
 	
 	// BLOCKS
 	private static void registerBlockState(Identifier advancementIdentifier, BlockState sourceBlockState, BlockState targetBlockState) {
-		if(sourceBlockState.isAir() || targetBlockState.isAir()) {
+		if(sourceBlockState.isAir()) {
 			Revelationary.logError("Trying to register invalid block cloak. Advancement: " + advancementIdentifier
 					+ " Source Block: " + Registry.BLOCK.getId(sourceBlockState.getBlock())
 					+ " Target Block: " + Registry.BLOCK.getId(targetBlockState.getBlock()));
