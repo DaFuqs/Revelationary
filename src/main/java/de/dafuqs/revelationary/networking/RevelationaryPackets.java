2702b81fb24f62e78cd66be6cb913f34aea51c51
imreallybadatnames <pizzagtav@gmail.com>
2024-04-30 18:39:37 +1100

    simplify packet buf store

--- src/main/java/de/dafuqs/revelationary/networking/RevelationaryPackets.java
+++ src/main/java/de/dafuqs/revelationary/networking/RevelationaryPackets.java
@@ -1,6 +1,7 @@
 package de.dafuqs.revelationary.networking;
 
 import de.dafuqs.revelationary.Revelationary;
+import net.minecraft.network.PacketByteBuf;
 import net.minecraft.network.RegistryByteBuf;
 import net.minecraft.network.codec.PacketCodec;
 import net.minecraft.network.packet.CustomPayload;
@@ -12,13 +13,13 @@ public class RevelationaryPackets {
 
 	// 1.20.5 port note: this whole buf-juggling thing is an obvious workaround for the fact that RevelationRegistry
 	// relies heavily on reading the buffer straight up
-	public record RevelationSync(RegistryByteBuf bufCopy) implements CustomPayload {
+	public record RevelationSync(PacketByteBuf bufCopy) implements CustomPayload {
 		public static final PacketCodec<RegistryByteBuf, RevelationSync> CODEC = CustomPayload.codecOf(RevelationSync::write, RevelationSync::read);
 		public static final CustomPayload.Id<RevelationSync> ID = new Id<>(REVELATION_SYNC);
 
 		private static RevelationSync read(RegistryByteBuf buf) {
 			// copy buffer to retain it for RevelationRegistry reading
-			RegistryByteBuf copy = new RegistryByteBuf(buf.copy(), buf.getRegistryManager());
+			PacketByteBuf copy = new PacketByteBuf(buf.copy());
 			// skip all the readable bytes in the original buffer to prevent decoder exception
 			buf.skipBytes(buf.readableBytes());
 			return new RevelationSync(copy);
--- src/main/java/de/dafuqs/revelationary/networking/RevelationaryS2CPacketSenders.java
+++ src/main/java/de/dafuqs/revelationary/networking/RevelationaryS2CPacketSenders.java
@@ -4,7 +4,6 @@ import de.dafuqs.revelationary.RevelationRegistry;
 import net.fabricmc.fabric.api.networking.v1.PacketByteBufs;
 import net.fabricmc.fabric.api.networking.v1.ServerPlayNetworking;
 import net.minecraft.network.PacketByteBuf;
-import net.minecraft.network.RegistryByteBuf;
 import net.minecraft.server.network.ServerPlayerEntity;
 
 public class RevelationaryS2CPacketSenders {
@@ -12,8 +11,7 @@ public class RevelationaryS2CPacketSenders {
 	public static void sendRevelations(ServerPlayerEntity player) {
 		PacketByteBuf packetByteBuf = PacketByteBufs.create();
 		RevelationRegistry.write(packetByteBuf);
-		// silly packet handling workaround
-		ServerPlayNetworking.send(player, new RevelationaryPackets.RevelationSync(new RegistryByteBuf(packetByteBuf, null)));
+		ServerPlayNetworking.send(player, new RevelationaryPackets.RevelationSync(packetByteBuf));
 	}
 	
 }
