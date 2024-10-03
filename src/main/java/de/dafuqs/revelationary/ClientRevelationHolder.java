6254e340ea7f0160226090659dc44d10cf23ae6a
Athebyne <30442287+f-raZ0R@users.noreply.github.com>
2024-10-03 10:51:49 -0400

    Make ClientRevelationHolder more extensible
    for better use by other mods

--- src/main/java/de/dafuqs/revelationary/ClientRevelationHolder.java
+++ src/main/java/de/dafuqs/revelationary/ClientRevelationHolder.java
@@ -120,10 +120,17 @@ public class ClientRevelationHolder {
 	}
 	
 	// BLOCKS
-	private static void cloak(BlockState blockState) {
+	public static void cloak(BlockState blockState) {
 		activeBlockStateSwaps.add(blockState);
 		if (blockState instanceof RevelationAware revelationAware) {
 			revelationAware.onCloak();
+		}
+	}
+
+	public static void uncloak(BlockState blockState) {
+		activeBlockStateSwaps.remove(blockState);
+		if (blockState instanceof RevelationAware revelationAware) {
+			revelationAware.onUncloak();
 		}
 	}
 	
@@ -142,12 +149,24 @@ public class ClientRevelationHolder {
 			return blockState;
 		}
 	}
+
+	public static Set<BlockState> getBlockCloaks() {
+		return activeBlockStateSwaps;
+	}
+
 	
 	// ITEMS
-	private static void cloak(Item item) {
+	public static void cloak(Item item) {
 		activeItemSwaps.add(item);
 		if (item instanceof RevelationAware revelationAware) {
 			revelationAware.onCloak();
+		}
+	}
+
+	public static void uncloak(Item item) {
+		activeItemSwaps.remove(item);
+		if (item instanceof RevelationAware revelationAware) {
+			revelationAware.onUncloak();
 		}
 	}
 	
@@ -161,6 +180,10 @@ public class ClientRevelationHolder {
 		} else {
 			return item;
 		}
+	}
+
+	public static Set<Item> getItemCloaks() {
+		return activeItemSwaps;
 	}
 	
 	public static void cloakAll() {
