44374c2350f874d135b2aa803e0ff112c0021305
DaFuqs <dominik.m.schmidt@googlemail.com>
2024-11-11 11:33:23 +0100

    1.21.1; update mod compat

---
+++ src/main/java/de/dafuqs/revelationary/compat/jei/RevelationaryJEIPlugin.java
@@ -0,0 +1,55 @@
+package de.dafuqs.revelationary.compat.jei;
+
+import de.dafuqs.revelationary.Revelationary;
+import de.dafuqs.revelationary.api.revelations.CloakSetChanged;
+import de.dafuqs.revelationary.config.RevelationaryConfig;
+import mezz.jei.api.IModPlugin;
+import mezz.jei.api.constants.VanillaTypes;
+import mezz.jei.api.runtime.IJeiRuntime;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.Identifier;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class RevelationaryJEIPlugin implements IModPlugin {
+    private IJeiRuntime runtime;
+    private Set<Item> stacksCache;
+
+    public RevelationaryJEIPlugin() {
+        if (!RevelationaryConfig.get().HideCloakedEntriesFromRecipeViewers) return;
+        CloakSetChanged.EVENT.register((added, removed, newStacks) -> {
+            stacksCache = newStacks;
+            if (runtime != null) {
+                var manager = runtime.getIngredientManager();
+                if(added != null && !added.isEmpty())
+                    manager.removeIngredientsAtRuntime(VanillaTypes.ITEM_STACK,
+                            added.stream().map(ItemStack::new).collect(Collectors.toList()));
+                if(removed != null && !removed.isEmpty())
+                    manager.addIngredientsAtRuntime(VanillaTypes.ITEM_STACK,
+                            removed.stream().map(ItemStack::new).collect(Collectors.toList()));
+            }
+        });
+    }
+
+    @Override
+    public @NotNull Identifier getPluginUid() {
+        return Identifier.of(Revelationary.MOD_ID, "jei_plugin");
+    }
+
+    @Override
+    public void onRuntimeAvailable(IJeiRuntime jeiRuntime) {
+        runtime = jeiRuntime;
+        if (!RevelationaryConfig.get().HideCloakedEntriesFromRecipeViewers) return;
+        if (stacksCache != null && !stacksCache.isEmpty()) runtime.getIngredientManager()
+                .removeIngredientsAtRuntime(VanillaTypes.ITEM_STACK,
+                        stacksCache.stream().map(ItemStack::new).collect(Collectors.toList()));
+    }
+
+    @Override
+    public void onRuntimeUnavailable() {
+        runtime = null;
+    }
+}
